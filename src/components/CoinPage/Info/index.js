3e6ae6849fa39cf72fb8a99bbf997ee0208058ca
Sayakb03 <sayakbhattacharjee212003@gmail.com>
2024-07-15 22:14:47 +0530

    a cryptocurrency dashboard

--- src/components/CoinPage/Info/index.js
+++ src/components/CoinPage/Info/index.js
@@ -1,26 +1,38 @@
 import React, { useState } from "react";
 import "./styles.css";
-function Info({ title, desc }) {
-  const shortDesc =
-    desc.slice(0, 300) +
-    "<br/><p style='color:var(--grey);cursor:pointer;'>Read More...</p>";
-  const longDesc =
-    desc + "<br/><p style='color:var(--grey);cursor:pointer;'>Read Less...</p>";
 
-  const [toggle, setToggle] = useState(false);
+function Info({ title, desc }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const shortDescription = (
+    <>
+      {desc.slice(0, 300)}
+      <br />
+      <p style={{ color: "var(--grey)", cursor: "pointer" }}>Read More...</p>
+    </>
+  );
+
+  const longDescription = (
+    <>
+      {desc}
+      <br />
+      <p style={{ color: "var(--grey)", cursor: "pointer" }}>Read Less...</p>
+    </>
+  );
 
   return (
     <div className="grey-wrapper info-component">
       <h1>{title}</h1>
       <p
         dangerouslySetInnerHTML={{
-          __html: desc.length >= 300 ? (toggle ? longDesc : shortDesc) : desc,
+          __html: desc.length >= 300 ? (isExpanded ? longDescription : shortDescription) : desc,
         }}
         className="info-p"
-        onClick={() => setToggle(!toggle)}
+        onClick={() => setIsExpanded(!isExpanded)}
       />
     </div>
   );
 }
 
 export default Info;
+
